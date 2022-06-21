6e54181ebb6878a319e1de49d41a95a89d77b6f2
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-21 14:25:57 -0500

    add: about component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+
 import Mode from "./components/Mode/Mode";
 import Header from "./components/Header/Header";
+import { About } from "./components/About/About";
 
 function App() {
   return (
     <>
       <Mode />
       <Header />
+      <About />
     </>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,25 @@
+import "./About.css";
+import iconImage from "../../images/logo.jpg";
+
+export const About = () => {
+  return (
+    <section className="container-about">
+      <div className="about-left">
+        <h2 className="white-text">React </h2>
+        <p>
+          Hola Mi nombre es Luis y me encanta Desarrollar productos increibles
+          para el mundo, Llevo desarro llando hace mas de 2 años usando Reactjs
+          and this is <span className="purple-text">Portoflio.</span>
+        </p>
+        <ul>
+          <li>React Developer</li>
+          <li>Ui/Ux Designer</li>
+          <li>Backedn Developer</li>
+        </ul>
+      </div>
+      <div className="about-right">
+        <img src={iconImage} alt="" />
+      </div>
+    </section>
+  );
+};
