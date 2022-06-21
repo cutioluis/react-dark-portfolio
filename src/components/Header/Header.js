117129d99a5873f34a9af045895edeed3c02c392
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-21 13:04:48 -0500

    add: component header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <section className="header">
+      <header>
+        <h1 className="header-title"> Hello.</h1>
+        <div className="header-title-box">
+          ⚛ React developer based in Ecuador!
+        </div>
+      </header>
+    </section>
+  );
+}
