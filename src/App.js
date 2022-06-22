0105c66ca4deb8b1d653e64b5d75f25e986c8972
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-22 12:59:51 -0500

    fix: problems responsive

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Mode from "./components/Mode/Mode";
 import Header from "./components/Header/Header";
 import { About } from "./components/About/About";
+import { Bio } from "./components/Bio/Bio";
 import { Footer } from "./components/Footer/Footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Mode />
       <Header />
       <About />
+      <Bio />
       <Footer />
     </>
   );
