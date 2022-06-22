e97d555b7bfc8ad50c293bf100774d8f1536e1e2
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-22 15:32:54 -0500

    add: components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import { About } from "./components/About/About";
 import { Bio } from "./components/Bio/Bio";
 import { Footer } from "./components/Footer/Footer";
+import { BioDetails } from "./components/BioDetails/BioDetails";
 
 function App() {
   return (
