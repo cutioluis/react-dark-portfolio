be2cc7d98849e0fd41235ce29dcaede454696a14
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-22 12:58:30 -0500

    fix: mode component to navbar with links

--- src/components/Mode/Mode.js
+++ src/components/Mode/Mode.js
@@ -1,10 +1,20 @@
 import "./Mode.css";
 import { RiSunFill } from "react-icons/ri";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Mode() {
   return (
-    <section className="container-mode">
-      <p>Light Mode</p>
+    <section className=" container-mode">
+      <div className="mode-left">
+        <ul>
+          <li>Works</li>
+          <li className="left-source">
+            <AiFillGithub className="source-icon" />
+            Source
+          </li>
+          <li>Me</li>
+        </ul>
+      </div>
       <div className="container-icon">
         <RiSunFill className="mode-icon" />
       </div>
