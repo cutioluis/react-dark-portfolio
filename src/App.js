960cdf155b0a656478da1829a7bde03276d109f8
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-24 20:01:42 -0500

    add: tooltip component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+import ReactTooltip from "react-tooltip";
 
 import Mode from "./components/Mode/Mode";
 import Header from "./components/Header/Header";
 import { About } from "./components/About/About";
 import { Bio } from "./components/Bio/Bio";
 import { Footer } from "./components/Footer/Footer";
-import { BioDetails } from "./components/BioDetails/BioDetails";
 
 function App() {
   return (
