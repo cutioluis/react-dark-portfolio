12ff115a2a5ccead371dc7178e9b7504d3e3f1b7
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-06-24 20:00:55 -0500

    edit: emojis about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ export const About = () => {
   return (
     <section className="container container-about">
       <div className="about-left">
-        <span className="purple-text">Luis Cutiopala ✋</span>
+        <span className="purple-text">✋Luis Cutiopala </span>
         <p>
           Empece en el mundo del desarrollo cuando tenia 14 años jugando con c++
           creando juegos en la terminal, cuando descubri el desarrollo web
@@ -13,6 +13,7 @@ export const About = () => {
           actualmente trabajo con <span className="white-text">Reactjs</span> y{" "}
           <span className="white-text">Nodejs</span>
         </p>
+        <a href="">See work</a>
       </div>
       <div className="about-right">
         <img src={iconImage} alt="" />
